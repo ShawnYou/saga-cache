ffae3011d038504072a369ede55fbe32bfc4e17e
shawn_lin <272840845@qq.com>
2018-06-21 21:53:39 +0800

    init project

---
+++ src/main/java/com/ysl/Jpa4cacheApplication.java
@@ -0,0 +1,12 @@
+package com.ysl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa4cacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa4cacheApplication.class, args);
+	}
+}
