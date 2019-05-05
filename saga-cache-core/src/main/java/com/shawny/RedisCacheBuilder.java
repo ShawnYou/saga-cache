56074036aed732a3f82ed2fc5cdfdd440856f29a
shawn_lin <272840845@qq.com>
2019-05-05 21:39:46 +0800

    add config

--- saga-cache-core/src/main/java/com/shawny/RedisCacheBuilder.java
+++ saga-cache-core/src/main/java/com/shawny/RedisCacheBuilder.java
@@ -1,7 +1,16 @@
 package com.shawny;
 
+
+import com.sun.tools.javac.util.Assert;
+
 /**
  * Created by shawn_lin on 2019/5/1.
  */
 public class RedisCacheBuilder extends AbstractRemoteCacheBuilder{
+    private RedisCacheConfig redisCacheConfig;
+
+    public RedisCacheBuilder(RedisCacheConfig redisCacheConfig){
+        Assert.checkNonNull(redisCacheConfig);
+        this.redisCacheConfig = redisCacheConfig;
+    }
 }
