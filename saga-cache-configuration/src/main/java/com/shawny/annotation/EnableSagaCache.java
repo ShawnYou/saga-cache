448de6d8aec207cea5808421f5b120f2c2781a83
shawn_lin <272840845@qq.com>
2019-05-09 22:21:41 +0800

    add config selector

---
+++ saga-cache-configuration/src/main/java/com/shawny/annotation/EnableSagaCache.java
@@ -0,0 +1,18 @@
+package com.shawny.annotation;
+
+import com.shawny.configuration.SagaCacheSeletor;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by shawn_lin on 2019/5/9.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
+@Documented
+@Import(SagaCacheSeletor.class)
+public @interface EnableSagaCache {
+}
---
+++ saga-cache-configuration/src/main/java/com/shawny/configuration/SagaCacheSeletor.java
@@ -0,0 +1,7 @@
+package com.shawny.configuration;
+
+/**
+ * Created by shawn_lin on 2019/5/9.
+ */
+public class SagaCacheSeletor {
+}
