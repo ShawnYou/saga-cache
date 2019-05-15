d03fc42edf60f4545cd199a98339313679a400ba
shawn_lin <272840845@qq.com>
2019-05-15 21:02:20 +0800

    add requirement

---
+++ saga-cache-annotation/src/main/java/com/shawny/annotaion/CacheRefresh.java
@@ -0,0 +1,14 @@
+package com.shawny.annotaion;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by shawn_lin on 2019/5/15.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
+@Documented
+public @interface CacheRefresh {
+    long interval();
+}
--- saga-cache-configuration/src/main/java/com/shawny/aop/SagaCacheInterceptor.java
+++ saga-cache-configuration/src/main/java/com/shawny/aop/SagaCacheInterceptor.java
@@ -4,9 +4,13 @@ package com.shawny.aop;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.core.BridgeMethodResolver;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.util.ClassUtils;
 
 import java.lang.reflect.Method;
+import java.util.Collection;
 
 
 /**
@@ -28,7 +32,10 @@ public class SagaCacheInterceptor implements MethodInterceptor {
     private Object execute(MethodInvocation invocation,Method method,Object[] args,Object target){
         Object object = invocation.getThis();
         Class<?> targetClass = target.getClass();
-        ClassUtils.getMostSpecificMethod(method,targetClass);
+        Method specificMethod = ClassUtils.getMostSpecificMethod(method,targetClass);
+        specificMethod = BridgeMethodResolver.findBridgedMethod(specificMethod);
+        Collection<CachePut> cachePuts = AnnotatedElementUtils.findAllMergedAnnotations(specificMethod, CachePut.class);
+        //Collection<CachePut> cachePuts = AnnotatedElementUtils.findAllMergedAnnotations(specificMethod, );
         return null;
     }
 }
