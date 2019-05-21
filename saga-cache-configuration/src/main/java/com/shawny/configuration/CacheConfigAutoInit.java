4132f6899c2947a93c0fad55c868b93855ebe6fe
shawn_lin <272840845@qq.com>
2019-05-21 22:56:05 +0800

    add ymal configur

--- saga-cache-configuration/src/main/java/com/shawny/configuration/CacheConfigAutoInit.java
+++ saga-cache-configuration/src/main/java/com/shawny/configuration/CacheConfigAutoInit.java
@@ -3,14 +3,21 @@ package com.shawny.configuration;
 import com.shawny.config.ConfigMap;
 import com.shawny.config.External;
 import com.shawny.config.Internal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * Created by shawn_lin on 2019/5/20.
  */
 @Configuration
+@PropertySource(value = "classpath:/application.yml",factory = YmalPropertyLoadFactory.class)
 public class CacheConfigAutoInit extends AbstractCacheConfigAutoInit {
+
+    @Autowired
+    private ConfigurableEnvironment environment;
 
     @Bean
     public External externalConfig(){
