9dcf19121f3423276cc94920ce7f61f340ad91b3
shawn_lin <272840845@qq.com>
2019-05-28 21:26:59 +0800

    find how to test

---
+++ saga-cahce-test/src/main/java/com/shawny/Test.java
@@ -0,0 +1,12 @@
+package com.shawny;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by shawn_lin on 2019/5/28.
+ */
+
+@Configuration
+public class Test {
+
+}
--- saga-cahce-test/src/test/java/com/shawny/AutoInitTest.java
+++ saga-cahce-test/src/test/java/com/shawny/AutoInitTest.java
@@ -1,10 +1,20 @@
 package com.shawny;
 
-import org.junit.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by shawn_lin on 2019/5/28.
  */
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Test.class)
 public class AutoInitTest {
+    @Before
+    public void init(){
+
+    }
 
 }
