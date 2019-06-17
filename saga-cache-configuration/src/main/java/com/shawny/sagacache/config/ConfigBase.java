29cfcb1d80a7d350e8353ec8935c7722d34e9e59
shawn_lin <272840845@qq.com>
2019-06-17 22:45:32 +0800

    add lombok

--- saga-cache-configuration/src/main/java/com/shawny/sagacache/config/ConfigBase.java
+++ saga-cache-configuration/src/main/java/com/shawny/sagacache/config/ConfigBase.java
@@ -1,7 +1,14 @@
 package com.shawny.sagacache.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by shawn_lin on 2019/6/1.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConfigBase {
 }
--- saga-cache-configuration/src/main/java/com/shawny/sagacache/config/SagaCacheProperties.java
+++ saga-cache-configuration/src/main/java/com/shawny/sagacache/config/SagaCacheProperties.java
@@ -1,9 +1,12 @@
 package com.shawny.sagacache.config;
 
+import lombok.Data;
+
 /**
  * Created by shawn_lin on 2019/6/10.
  */
 
+@Data
 public class SagaCacheProperties {
     private String type;
     private String name;
