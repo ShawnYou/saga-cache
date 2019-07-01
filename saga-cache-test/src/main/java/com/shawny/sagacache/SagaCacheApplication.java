096eaced91c796a29f8cfb4ad48a0002a6afd093
Aaron You <aarony@synnex.com>
2019-07-01 17:33:05 +0800

    add dependencymanagement about maven pom

--- saga-cache-test/src/main/java/com/shawny/sagacache/SagaCacheApplication.java
+++ saga-cache-test/src/main/java/com/shawny/sagacache/SagaCacheApplication.java
@@ -1,15 +1,11 @@
 package com.shawny.sagacache;
 
-import com.shawny.sagacache.annotation.EnableSagaCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- */
 @SpringBootApplication
-@EnableSagaCache
-public class TestApplication {
+public class SagaCacheApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class, args);
+        SpringApplication.run(SagaCacheApplication.class,args);
     }
 }
