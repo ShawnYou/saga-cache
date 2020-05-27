e268a19876af6fe565e3dc1799207b69d12f80ad
ShawnYou <272840845@qq.com>
2020-05-27 12:03:04 +0800

    restart design the saga cache

---
+++ src/main/java/com/shawny/saga/CaffeineCache.java
@@ -0,0 +1,19 @@
+package com.shawny.saga;
+
+
+public class CaffeineCache<K,V> implements Cache<K,V>{
+    @Override
+    public V get(K k) {
+        return null;
+    }
+
+    @Override
+    public void add(K k) {
+
+    }
+
+    @Override
+    public void delete(K k) {
+
+    }
+}
