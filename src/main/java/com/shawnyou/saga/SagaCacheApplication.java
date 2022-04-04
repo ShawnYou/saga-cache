b43f4c5472e430f17c3080262b9676a7366a3490
youshuanglin <youshuanglin@migu.cn>
2022-04-04 16:50:53 +0800

    init project

---
+++ src/main/java/com/shawnyou/saga/SagaCacheApplication.java
@@ -0,0 +1,13 @@
+package com.shawnyou.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaCacheApplication.class, args);
+    }
+
+}
